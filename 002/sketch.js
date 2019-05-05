a2a7cf1f8d8592c6ef06fec2e308b0eb3391d7f0
TimFrendo <tirnagog123@gmail.com>
2019-05-06 00:27:46 +0600

    add Map

--- 002/sketch.js
+++ 002/sketch.js
@@ -1,42 +1,73 @@
-let V = [];
-let vertexs = ['a', 'b', 'c', 'd', 'e', 'f'];
-let edges = [
+/*let boundaries = [
     ['a', 'b'],
-    ['a', 'd'],
+    ['a', 'e'],
     ['b', 'c'],
-    ['b', 'e'],
-    ['c', 'f'],
-    ['d', 'e']
-];
+    ['b', 'f'],
+    ['c', 'd'],
+    ['c', 'i'],
+    ['d', 'j'],
+    ['e', 'f'],
+    ['f', 'l'],
+    ['j', 'n'],
+    ['k', 'l'],
+    ['l', 'm'],
+    ['m', 'p'],
+    ['o', 'p']
+];*/
+ let boundaries = []; 
 
-function setup() {
-    createCanvas(700, 700);
-    background(0);
-    for (let i = 0; i < vertexs.length; i++) {
-        V[i] = new Vertex(random(width), random(height));
-        V[i].setName(vertexs[i], i);
-        print(V[i].dict.name);
-        V[i].draw();
+  function preload() {
+    boundaries = loadStrings('boundaries.txt');
+  }
+
+  let b = new Map();
+  let boundary = [];
+  
+  let adj = [];
+  let k = 0;
+  
+  function setup() {
+    createCanvas(400, 400);
+    //print(boundaries)
+
+    for (let line of boundaries){
+      let points = split(line, ' ');
+      for(let point of points){
+        if(!boundary.includes(point))
+          boundary.push(point)
+      }
     }
-
-    for(let e of edges){        
-        let v1;
-        let v2;
-        //print()
-        for(let i = 0; i < vertexs.length; i++){
-            if(e[0] == V[i].dict.name)
-                v1 = V[i];
-            if(e[1] == V[i].dict.name)
-                v2 = V[i];
-        }
-        v1.drawEdge(v2);
-    }
-
-
-}
-
-function draw() {
     
+  
+    for (let i of boundary) {
+      b.set(i, b.size);      
+    }
+    for(let i = 0; i < boundary.length; i++){
+     adj[i] = []; 
+    }
+    
+    for(let line of boundaries){
+       let s = split(line, ' ');       
+      addEdge(index(s[0]), index(s[1]))
+    }
+    print(boundary)
+    print(b)
+   
+    print(adj)
+    
+  }
+  
+  function draw() {
+    background(220);
+  }
+  
+  function addEdge(v, w) {
+    adj[v].push(w)
+    adj[w].push(v)
+  }
+  
+  function index(s){
+    return b.get(s)
+  }
 
-}
-
+  
